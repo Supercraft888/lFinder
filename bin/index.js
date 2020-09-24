682124b0218a9e4e6b792ace999b1b5a79d36524
Alexander Hugh <43384957+Supercraft888@users.noreply.github.com>
2020-09-24 01:02:49 -0400

    Add files via upload

--- bin/index.js
+++ bin/index.js
@@ -19,46 +19,79 @@ fetch(url).then(res => {
     throw new Error(invalid input!  Stopping the CLI before you break it more!)
   }
 }
-
 */
 
 const chalk = require("chalk");
 const boxen = require("boxen");
 const yargs = require("yargs");
-const fs = require ("fs");
-const fetch = require ("node-fetch");
+const fs = require("fs");
+const fetch = require("node-fetch");
+const { url } = require("inspector");
+const { count } = require("console");
 
 const regexForURLs = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
 
-let lineArray = [];
-let urlArray = [];
+let urlLinks = [];
 
 //This is the argument passer
 const options = yargs
-    .usage("Usage: -n <name>")
-    .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
-    .argv;
+  .usage("Usage: -n <name>")
+  .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+  .argv;
 
 //how to read the file entered in the argument
+fs.readFile(options.name, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    let lineArray = data.toString().split("\n");
+    console.log("Number of lines in file is: ", lineArray.length);
 
-fs.readfile(options.name, function(err, data){
-    if (err){
-        console.log(err);
-    } else {
-        lineArray.push(data.toString().split("\n"));
-    }
+    let urlArray = lineArray.filter(line => line.includes("https:") || line.includes("http:") || line.includes("HTTPS:") || line.includes("HTTP:"));
+    console.log("The number of URLs in this file: ", urlArray.length);
+
+    for (let i = 0; i < urlArray.length; i++) {
+      let res = urlArray[i].match(regexForURLs);
+      if (res) {
+        urlLinks = urlLinks.concat(res);
+      }
+    };
+    console.log(urlLinks);
+    
+    urlLinks.forEach(async url => {
+      try {
+        const urlIndiv = await fetch(url, {method: "head"});
+        if (urlIndiv.status === 200) {
+          console.log(chalk.greenBright("good: ", url));
+        }
+        else if (urlIndiv.status === 400 || urlIndiv.status === 404) {
+          console.log(chalk.redBright("bad: ", url));
+        }
+        else {
+          console.log(chalk.grey("unknown: ", url));
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    });
+  }
 });
 
+
+
+
+
+
+
 var greeting = chalk.white.bold(`${options.name}!`);
-//greeting = chalk.white.bold(greeting);
 
 //Making box for text, not necessary in the long run.
 const boxenOptions = {
-    padding: 1,
-    margin: 1,
-    borderStyle: "round",
-    borderColor: "green",
-    backgroundColor: "#555555"
+  padding: 1,
+  margin: 1,
+  borderStyle: "round",
+  borderColor: "green",
+  backgroundColor: "#555555"
 };
 const msgBox = boxen(greeting, boxenOptions);
 
